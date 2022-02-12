81fd5c098c1b4ed5056e743c6eb21e0e7b67aeda
bryan <bryan@Outer-Rim.bwrouter>
2022-02-12 17:20:08 -0600

    agregar katas 1 a 7

---
+++ Modulo 0 - Preparacion del entorno de trabajo/sumas.py
@@ -0,0 +1,33 @@
+# programa de sumar
+from datetime import date as dt
+#sum = 1 + 2
+#print(sum)
+
+sum = 1 / 2 # 3
+product = sum * 2
+print(product)
+
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+print(type(distancia_a_alfa_centauri))
+
+print('--------------------------')
+var = 6
+var *= 3
+print(var)
+print('--------------------------')
+print("Hoy es " + str(dt.today()))
+print('--------------------------')
+
+print('Programa de bienvenida')
+#name = input("Ingrese su nombre: ")
+#print("buenas noches, " + name)
+
+
+print('--------------------------')
+print("Calculator")
+first_number = int(input("primer numero: "))
+second_number = int(input("segundo numero: "))
+print(first_number + second_number)
---
+++ Módulo 2 - Crear y administrar proyectos/Modulo2Katas.md
@@ -0,0 +1,9 @@
+# Módulo 2 - Crear y administrar proyectos
+
+### Crear un entorno virtual mediante venv:
+
+![title](images/kata_2_cap_1_2022-02-08_20-13-53.png)
+
+### Activar el entorno virtual e instalar libreria dateutil:
+
+![title](images/kata_2_cap_2_2022-02-08_20-15-00.png)
